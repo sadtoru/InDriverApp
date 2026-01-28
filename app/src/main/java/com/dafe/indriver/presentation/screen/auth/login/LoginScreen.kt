b2f98db71222ec04c58e6abcf15c74d75bd3d732
Gothman240 <paez.federico96@gmail.com>
2026-01-28 15:58:35 -0300

    Update LoginScreen UI with email and password fields.

--- app/src/main/java/com/dafe/indriver/presentation/screen/auth/login/LoginScreen.kt
+++ app/src/main/java/com/dafe/indriver/presentation/screen/auth/login/LoginScreen.kt
@@ -2,16 +2,31 @@ package com.dafe.indriver.presentation.screen.auth.login
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun LoginScreen() {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
     Scaffold { paddingValues ->
         Box(
             modifier = Modifier
@@ -19,14 +34,27 @@ fun LoginScreen() {
                 .background(
                     brush = Brush.linearGradient(
                         colors = listOf(
-                            Color(0xFFA8DEE6),
-                            Color(0xFF74B9FF)
+                            Color(0xFF74B9FF),
+                            Color(0xFFA8DEE6)
                         )
                     )
                 )
                 .padding(paddingValues)
-        ){
-
+        ) {
+            Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+                Text("InDrive", color = Color.White, fontSize = 34.sp, fontWeight = FontWeight.Bold)
+                Spacer(modifier = Modifier.weight(1f))
+                Text("Login", color = Color.White, fontSize = 24.sp, fontWeight = FontWeight.Bold)
+                Spacer(modifier = Modifier.weight(.1f))
+                TextField(value = email, onValueChange = { email = it }, label = { Text("Email") })
+                TextField(value = password, onValueChange = { password = it }, label = { Text("Password") })
+                Spacer(modifier = Modifier.weight(1f))
+                Button(onClick = {} ) {
+                    Text("Login")
+                }
+                Text("Don't have an account? Sign up")
+                Spacer(modifier = Modifier.weight(.1f))
+            }
         }
     }
 }
